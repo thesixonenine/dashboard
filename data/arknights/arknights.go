aed4da1f4b542171dddbceff90214c530631b1a1
Simple <huyang1994822@gmail.com>
2024-03-07 22:46:12 +0800

    update

--- data/arknights/arknights.go
+++ data/arknights/arknights.go
@@ -194,5 +194,6 @@ func (w Wish) String() string {
 }
 
 func TsToTime(ts int64) string {
-	return time.Unix(ts, 0).Format("2006-01-02 03:04:05")
+	location, _ := time.LoadLocation("Asia/Shanghai")
+	return time.Unix(ts, 0).In(location).Format("2006-01-02 15:04:05")
 }
