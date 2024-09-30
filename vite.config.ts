eb86ce880cd10a3e34986a00f2877013321d7285
Simple <huyang1994822@gmail.com>
2024-09-30 15:29:17 +0800

    switching sass API to modern

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,11 @@ export default defineConfig({
         }
     },
     css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler'
+            }
+        },
         postcss: {
             plugins: [tailwindcss, autoprefixer]
         }
