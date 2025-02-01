68aa44ebf1f719e36768f1352f2366dfc32e6d47
Simple <huyang1994822@gmail.com>
2025-02-01 21:01:21 +0800

    update arknights

--- data/star-rail-wish/star-rail-wish.go
+++ data/star-rail-wish/star-rail-wish.go
@@ -18,7 +18,7 @@ import (
 	"time"
 )
 
-const WishHistoryFilePath = "C:\\Program Files\\Star Rail\\Game\\StarRail_Data\\webCaches\\2.31.2.0\\Cache\\Cache_Data\\data_2"
+const WishHistoryFilePath = "C:\\Program Files\\Star Rail\\Game\\StarRail_Data\\webCaches\\2.32.0.0\\Cache\\Cache_Data\\data_2"
 const JSONFilePath = "../../src/assets/data/star-rail-wish.json"
 
 var re = regexp.MustCompile(`\p{C}`)
